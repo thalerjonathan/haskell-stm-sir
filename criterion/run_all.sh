df8b23a9f725b2230c91a72a4b45f500f8d910c9
Jonathan Thaler <thaler.jonathan@gmail.com>
2019-12-03 09:32:27 +0100

    all cores experiments, except seq run

---
+++ criterion/run_all.sh
@@ -0,0 +1,20 @@
+#cd sir-io/
+#stack build
+#sh run.sh
+#cd ..
+
+cd sir-io-atomic/
+sh run.sh
+cd ..
+
+cd sir-io-rw/
+sh run.sh
+cd ..
+
+cd sir-stm/
+sh run.sh
+cd ..
+
+sd sir-seq/
+sh run.sh
+cd ..
