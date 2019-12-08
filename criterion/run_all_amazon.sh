d80e4a57ed3e911629dd53beb490e2022aab0790
Jonathan Thaler <thaler.jonathan@gmail.com>
2019-12-08 15:48:14 +0100

    added sir amazon experiment shellscripts

---
+++ criterion/run_all_amazon.sh
@@ -0,0 +1,11 @@
+cd sir-io-atomic/
+sh run_amazon.sh
+cd ..
+
+cd sir-io-rw/
+sh run_amazon.sh
+cd ..
+
+cd sir-tvar/
+sh run_amazon.sh
+cd ..
---
+++ criterion/sir-io-rw/run_amazon.sh
@@ -0,0 +1,7 @@
+stack exec -- sir-io-rw sir-io-rw-agents:16/51x51 -o sir-io-rw_amazon_51x51_16.html +RTS -N16
+
+stack exec -- sir-io-rw sir-io-rw-agents:32/51x51 -o sir-io-rw_amazon_51x51_32.html +RTS -N32
+
+stack exec -- sir-io-rw sir-io-rw-agents:16/251x251 -o sir-io-rw_amazon_251x251_16.html +RTS -N16
+
+stack exec -- sir-io-rw sir-io-rw-agents:32/251x251 -o sir-io-rw_amazon_251x251_32.html +RTS -N32
