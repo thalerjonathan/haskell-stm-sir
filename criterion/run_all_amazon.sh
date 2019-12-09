c53efc935dffefa30f0d1ba4034e8cabd0b554eb
Jonathan Thaler <thaler.jonathan@gmail.com>
2019-12-09 22:13:28 +0100

    also running amazon 64 cores

--- criterion/run_all_amazon.sh
+++ criterion/run_all_amazon.sh
@@ -2,10 +2,6 @@ cd sir-io-atomic/
 sh run_amazon.sh
 cd ..
 
-cd sir-io-rw/
-sh run_amazon.sh
-cd ..
-
 cd sir-tvar/
 sh run_amazon.sh
 cd ..
--- criterion/sir-io-atomic/run_amazon.sh
+++ criterion/sir-io-atomic/run_amazon.sh
@@ -1,7 +1,11 @@
-stack exec -- sir-io-atomic sir-io-atomic-cores:16/51x51 -o sir-io-atomic_amazon_51x51_16.html +RTS -N16
+#stack exec -- sir-io-atomic sir-io-atomic-cores:16/51x51 -o sir-io-atomic_amazon_51x51_16.html +RTS -N16
 
-stack exec -- sir-io-atomic sir-io-atomic-cores:32/51x51 -o sir-io-atomic_amazon_51x51_32.html +RTS -N32
+#stack exec -- sir-io-atomic sir-io-atomic-cores:32/51x51 -o sir-io-atomic_amazon_51x51_32.html +RTS -N32
 
-stack exec -- sir-io-atomic sir-io-atomic-agents:16/251x251 -o sir-io-atomic_amazon_251x251_16.html +RTS -N16
+stack exec -- sir-io-atomic sir-io-atomic-cores:64/51x51 -o sir-io-atomic_amazon_51x51_64.html +RTS -N64
 
-stack exec -- sir-io-atomic sir-io-atomic-agents:32/251x251 -o sir-io-atomic_amazon_251x251_32.html +RTS -N32
+#stack exec -- sir-io-atomic sir-io-atomic-agents:16/251x251 -o sir-io-atomic_amazon_251x251_16.html +RTS -N16
+
+#stack exec -- sir-io-atomic sir-io-atomic-agents:32/251x251 -o sir-io-atomic_amazon_251x251_32.html +RTS -N32
+
+stack exec -- sir-io-atomic sir-io-atomic-agents:64/251x251 -o sir-io-atomic_amazon_251x251_64.html +RTS -N64
