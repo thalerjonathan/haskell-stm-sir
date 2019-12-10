5577b98a8854edd8727e617d1bfdace70c34b4a8
Jonathan Thaler <thaler.jonathan@gmail.com>
2019-12-10 10:06:17 +0100

    amazon experiments

--- criterion/sir-tvar/run_amazon.sh
+++ criterion/sir-tvar/run_amazon.sh
@@ -1,11 +1,11 @@
-#stack exec -- sir-tvar sir-tvar-cores:16/51x51 -o sir-tvar_amazon_51x51_16.html +RTS -N16
+stack exec -- sir-tvar sir-tvar-cores:16/51x51 -o sir-tvar_amazon_51x51_16.html +RTS -N16
 
-#stack exec -- sir-tvar sir-tvar-cores:32/51x51 -o sir-tvar_amazon_51x51_32.html +RTS -N32
+stack exec -- sir-tvar sir-tvar-cores:32/51x51 -o sir-tvar_amazon_51x51_32.html +RTS -N32
 
-stack exec -- sir-tvar sir-tvar-cores:64/51x51 -o sir-tvar_amazon_51x51_64.html +RTS -N64
+#stack exec -- sir-tvar sir-tvar-cores:64/51x51 -o sir-tvar_amazon_51x51_64.html +RTS -N64
 
-#stack exec -- sir-tvar sir-tvar-agents:16/251x251 -o sir-tvar_amazon_251x251_16.html +RTS -N16
+stack exec -- sir-tvar sir-tvar-agents:16/251x251 -o sir-tvar_amazon_251x251_16.html +RTS -N16
 
-#stack exec -- sir-tvar sir-tvar-agents:32/251x251 -o sir-tvar_amazon_251x251_32.html +RTS -N32
+stack exec -- sir-tvar sir-tvar-agents:32/251x251 -o sir-tvar_amazon_251x251_32.html +RTS -N32
 
-stack exec -- sir-tvar sir-tvar-agents:64/251x251 -o sir-tvar_amazon_251x251_64.html +RTS -N64
+#stack exec -- sir-tvar sir-tvar-agents:64/251x251 -o sir-tvar_amazon_251x251_64.html +RTS -N64
